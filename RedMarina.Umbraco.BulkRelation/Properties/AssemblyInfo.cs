08696abc326f71c2a9962b7b1af57fe1c4fc6077
Darren Ferguson <darren@moriyama.co.uk>
2021-06-04 13:43:16 +0100

    Setup gitversion

--- RedMarina.Umbraco.BulkRelation/Properties/AssemblyInfo.cs
+++ RedMarina.Umbraco.BulkRelation/Properties/AssemblyInfo.cs
@@ -22,14 +22,3 @@ using System.Runtime.InteropServices;
 [assembly: Guid("3c8da624-2591-41c8-9481-044a976ce5a2")]
 
 // Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
